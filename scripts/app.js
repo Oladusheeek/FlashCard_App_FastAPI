ced072a220ce062f3a619be18e24ad9e5db7b55e
Oladik Oladik <sniperhydra@gmail.com>
2026-02-20 16:29:11 +0300

    Add creation and deletion of Sections from UI

--- scripts/app.js
+++ scripts/app.js
@@ -6,9 +6,18 @@ createApp({
             title: "My flashcard app",
             username: '',
             password: '',
-            token: null,
+            token: localStorage.getItem('flashcards_token') || null,
             error: null,
-            sections: []
+            sections: [],
+
+            showCreateSectionForm: false, // create section
+            newSectionTitle: '',
+
+            editingSectionId: null, // edit section
+            editingSectionTitle: '',
+
+            deletingSectionId: null, // delete section
+            deleteReady: false,
         }
     },
     mounted() {
@@ -40,6 +49,7 @@ createApp({
                     this.error = data.detail || 'Authorization error!'
                 } else {
                     this.token = data.access_token;
+                    localStorage.setItem('flashcards_token', this.token)
                     this.fetchSections();
                 }
             } 
@@ -71,6 +81,73 @@ createApp({
             } 
             catch (err) {
                 this.error = 'Cannot connect to the server. Ensure connectivity'
+            }
+        },
+        logout(){
+            this.token = null;
+            this.sections = [];
+            this.error = null;
+            localStorage.removeItem('flashcards_token');
+        },
+        //create_section
+        async create_section(){
+            const response = await fetch('http://localhost:8000/sections', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${this.token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title: this.newSectionTitle})
+            });
+
+            if(response.ok){
+                const newSection = await response.json();
+                this.sections.push(newSection);
+                this.newSectionTitle = ''
+            }
+        },
+        //Temp func for opening
+        open_section(id) {
+            pass;
+        },
+        // Edit section
+        startEdit(section) {
+            pass;
+        },
+        async delete_section(id){
+            const response = await fetch (`http://localhost:8000/sections/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${this.token}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if(response.ok){
+                this.sections = this.sections.filter(s => s.id !== id) //filtering deleted section
+            }
+        },
+        request_delete(id){
+            if(this.deletingSectionId !== id){
+                this.deletingSectionId = id;
+                this.deleteReady = false;
+
+                setTimeout(() => {
+                    if(this.deletingSectionId === id){
+                        this.deleteReady = true;
+                    }
+                }, 500);
+
+                setTimeout(() => {
+                    if(this.deletingSectionId === id){
+                        this.deletingSectionId = null;
+                        this.deleteReady = false;
+                    }
+                }, 2000);
+            } else if(this.deletingSectionId === id && this.deleteReady){
+                this.delete_section(id);
+                this.deletingSectionId = null;
+                this.deleteReady = false;
             }
         }
     }
